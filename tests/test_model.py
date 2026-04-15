668508099f30e20de7a9742226a8758ff53f8d0a
Ozair Shafique <bc090402541@gmail.com>
2026-04-16 00:50:47 +0200

    add tests for model loading and error handling in test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,7 @@
-from src.model_loader import predict
+from src.model_loader import predict, _load, AVAILABLE_MODELS
 import pytest
+from src import model_loader
+
 
 def test_predict():
     result = predict("This producgt is amazing!")
@@ -11,3 +13,25 @@ def test_predict_empty():
     with pytest.raises(ValueError):
         predict("")
 
+def test_model_not_found():
+    model_name = "svm"
+    assert model_name not in AVAILABLE_MODELS
+    with pytest.raises(ValueError):
+        _load(model_name)
+
+def test_load_model():
+    model = _load("logreg")
+    assert hasattr(model, "predict")
+    assert hasattr(model, "predict_proba")
+
+def test_load_model_file_not_found(tmp_path, monkeypatch):
+
+    model_loader._load.cache_clear()  # Clear lru_cache to ensure _load is called again
+    monkeypatch.setattr(model_loader, "MODEL_DIR", tmp_path)
+    excepted_path = tmp_path / "logreg_model.pkl"
+
+    if excepted_path.exists():
+        excepted_path.unlink()
+
+    with pytest.raises(FileNotFoundError):
+        model_loader._load("logreg")
